33673852ba854cef175e504195a29d558058eadc
Cláudio Duarte <90581914+claudiood@users.noreply.github.com>
2022-04-19 13:31:29 -0400

    update

--- Cronometro/latchRS.vhd
+++ Cronometro/latchRS.vhd
@@ -2,17 +2,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity latchRS is
-    port(pin_f1            :    in std_logic;
+    port(reset             :    in std_logic;
          q         	   :    out std_logic);
 end latchRS;
 
 architecture funcionality of latchRS is
 begin
-	process(pin_f1)
+	process(reset)
 	begin
-	if(pin_f1 = '0') then
+	if(reset = '0') then
 		q 	<= '0';
-			else if(pin_f1 = '1') then
+			else if(reset = '1') then
 				q <= '1';
 			end if;
 		end if;
