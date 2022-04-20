14f0ed3e9379b4bae8aba160f28ff22f56c54909
Cláudio Duarte <90581914+claudiood@users.noreply.github.com>
2022-04-20 16:15:27 -0300

    update

--- Cronometro/counternine.vhd
+++ Cronometro/counternine.vhd
@@ -13,26 +13,27 @@ end counternine;
 
 architecture funcionality of counternine is
 
-signal count : std_logic_vector(3 downto 0);
 	 
 begin
 	counter_bcd: process(counterclock, reset)
+	variable count : std_logic_vector(3 downto 0);
 	begin
 		if(reset = '0') then
+			nineout <= '1';
 			qb 	  	<= "0000";
-			count 	<= "0000";
+			count 	:= "0000";
 			else --if(reset = '1') then
 				if(counterclock'event and counterclock = '1') then
 
 					-- Conta até 9
 					if(count < "1001") then
-						count 	 <= count + '1';
+						count 	 := count + '1';
 						qb 	  	 <= count;
 						nineout  <= '0';
 						else if(count = "1001") then
 							nineout  <= '1';
 							qb 	  	 <= "0000";
-							count 	 <= "0000";
+							count 	 := "0000";
 						end if;
 					end if;
 				end if;
